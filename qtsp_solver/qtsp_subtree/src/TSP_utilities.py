616a4e6a1aa4cdb4264b73643a26a6ddc21aee2c
mstechly <michal.stechly@gmail.com>
2018-06-19 22:36:46 +0200

    Removed visualization from TSP_utilities.

--- qtsp_solver/qtsp_subtree/src/TSP_utilities.py
+++ qtsp_solver/qtsp_subtree/src/TSP_utilities.py
@@ -1,6 +1,6 @@
 import numpy as np
 import itertools
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 def create_nodes_array(N, seed=None):
     """
@@ -74,24 +74,24 @@ def calculate_cost(cost_matrix, solution):
     return cost
 
 
-def plot_solution(name, nodes_array, solution):
-    plt.scatter(nodes_array[:, 0], nodes_array[:, 1], s=200)
-    for i in range(len(nodes_array)):
-        plt.annotate(i, (nodes_array[i, 0] + 0.15, nodes_array[i, 1] + 0.15), size=16, color='r')
+# def plot_solution(name, nodes_array, solution):
+#     plt.scatter(nodes_array[:, 0], nodes_array[:, 1], s=200)
+#     for i in range(len(nodes_array)):
+#         plt.annotate(i, (nodes_array[i, 0] + 0.15, nodes_array[i, 1] + 0.15), size=16, color='r')
 
-    plt.xlim([min(nodes_array[:, 0]) - 1, max(nodes_array[:, 0]) + 1])
-    plt.ylim([min(nodes_array[:, 1]) - 1, max(nodes_array[:, 1]) + 1])
-    for i in range(len(solution) - 1):
-        A = solution[i]
-        B = solution[i + 1]
-        plt.plot([nodes_array[A, 0], nodes_array[B, 0]], [nodes_array[A, 1], nodes_array[B, 1]], c='r')
+#     plt.xlim([min(nodes_array[:, 0]) - 1, max(nodes_array[:, 0]) + 1])
+#     plt.ylim([min(nodes_array[:, 1]) - 1, max(nodes_array[:, 1]) + 1])
+#     for i in range(len(solution) - 1):
+#         A = solution[i]
+#         B = solution[i + 1]
+#         plt.plot([nodes_array[A, 0], nodes_array[B, 0]], [nodes_array[A, 1], nodes_array[B, 1]], c='r')
 
-    cost = calculate_cost(get_tsp_matrix(nodes_array), solution)
-    title_string = "Cost:" + str(cost)
-    title_string += "\n" + str(solution)
-    plt.title(title_string)
-    plt.savefig(name + '.png')
-    plt.clf()
+#     cost = calculate_cost(get_tsp_matrix(nodes_array), solution)
+#     title_string = "Cost:" + str(cost)
+#     title_string += "\n" + str(solution)
+#     plt.title(title_string)
+#     plt.savefig(name + '.png')
+#     plt.clf()
 
 def points_order_to_binary_state(points_order):
     """
